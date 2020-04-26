0bec259d28fcc0eeb9d284723a25fcd713675c71
JacekGlabDook <j.glab@dook.pro>
2020-04-27 01:48:25 +0200

    init

---
+++ src/base/i18n/locales/pl.js
@@ -0,0 +1,33 @@
+export default {
+  popup: {
+    title: "Zgłoś do sprawdzenia",
+    email_label: "Adres email do przesłania wyniku weryfikacji:",
+    submission_label: "Co w tej informacji budzi Twoją wątpliwość?",
+    send: "wyślij",
+    recaptcha_adnotation: "Ta strona jest chroniona przez reCAPTCHA i Google",
+    recaptcha_privacy: "Polityka prywatności",
+    recaptcha_terms: "Warunki korzystania z usługi.",
+    email_is_invalid: "Wygląda na to, że to niepoprawny adres email.",
+    email_is_required: "Podaj adres email",
+    description_is_required: "Dodaj opis znaleziska",
+    description_too_long: "Opis znaleziska jest za długi. skróć do 200 znaków",
+    success_msg_title: "Dziękujemy!",
+    success_msg_description:
+      "Gratulacje! Twoje zgłoszenie zostało wysłane. Nasi specjaliści zaczynają pracę. W zależności od złożoności problemu, zajmuje to od 4 do 24 godzin. Wyślemy Ci powiadomienie o wyniku weryfikacji.",
+    error_msg_title: "Ups! Coś poszło nie tak...",
+    error_msg_description: "Spróbuj ponownie później.",
+  },
+  alert: {
+    info_title: "Ta strona jest zaufana",
+    info_description: "Zawartość na tej stronie została zweryfikowana",
+    loading: "Ładowanie..",
+    blocked_title: "Ta strona została zablokowana",
+    error_title: "przepraszamy, nie możemy przetworzyć żądania",
+    no_marked_text: "Zaznacz tekst, który chcesz przesłać do sprawdzenia",
+    marked_text_too_long:
+      "Zaznaczony tekst jest zbyt długi. Skróć go do 250 znaków",
+    pathname_error_title: "Z tego miejsca zgłoszenie nie jest możliwe",
+    pathname_error_content:
+      "Pomóż nam lepiej zrozumieć źródło i kontekst wypowiedzi. Przejdź na stronę z treścią, którą chcesz zweryfikować, wchodząc na stronę artykułu, posta lub konkretnej wypowiedzi.",
+  },
+};
